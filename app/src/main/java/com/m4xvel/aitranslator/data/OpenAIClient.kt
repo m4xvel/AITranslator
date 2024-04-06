1a4acdc788052cb80112fee5b4c126774feb3ee0
m4xvel <mmenshov2004@gmail.com>
2024-04-06 16:25:46 +0300

    API changed

--- app/src/main/java/com/m4xvel/aitranslator/data/OpenAIClient.kt
+++ app/src/main/java/com/m4xvel/aitranslator/data/OpenAIClient.kt
@@ -4,6 +4,8 @@ import com.aallam.openai.api.chat.ChatCompletionRequest
 import com.aallam.openai.api.chat.chatCompletionRequest
 import com.aallam.openai.api.model.ModelId
 import com.aallam.openai.client.OpenAI
+import com.aallam.openai.client.OpenAIConfig
+import com.aallam.openai.client.OpenAIHost
 import com.m4xvel.aitranslator.BuildConfig
 
 internal class OpenAIClient(
@@ -11,11 +13,22 @@ internal class OpenAIClient(
 ) {
 
     private val token = BuildConfig.OPENAI_API_KEY
-    val openAI = OpenAI(token)
+    private val host = OpenAIHost(
+        baseUrl = "https://api.proxyapi.ru/openai/v1/chat"
+    )
+    private val config = OpenAIConfig(
+        host = host,
+        token = token
+    )
+    val openAI = OpenAI(config)
 
     private val modelId = ModelId("gpt-3.5-turbo")
 
-    fun getTransfer(sourceText: String, translatedText: String, text: String): ChatCompletionRequest {
+    fun getTransfer(
+        sourceText: String,
+        translatedText: String,
+        text: String
+    ): ChatCompletionRequest {
         val chatMessages = chatMessageProvider.createChatMessages(sourceText, translatedText, text)
         return chatCompletionRequest {
             model = modelId
--- app/src/main/java/com/m4xvel/aitranslator/data/repository/TransferRepositoryImpl.kt
+++ app/src/main/java/com/m4xvel/aitranslator/data/repository/TransferRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.m4xvel.aitranslator.data.repository
 
+import android.util.Log
 import com.m4xvel.aitranslator.data.OpenAIClient
 import com.m4xvel.aitranslator.domain.TransferRepository
 
@@ -12,12 +13,17 @@ internal class TransferRepositoryImpl(
         translatedText: String,
         text: String
     ): String? {
-        return openAIClient.openAI.chatCompletion(
-            openAIClient.getTransfer(
-                sourceText,
-                translatedText,
-                text
-            )
-        ).choices.first().message.content
+        return try {
+            openAIClient.openAI.chatCompletion(
+                openAIClient.getTransfer(
+                    sourceText,
+                    translatedText,
+                    text
+                )
+            ).choices.first().message.content
+        } catch (e: Exception) {
+            Log.e("Exception", "$e")
+            e.message.toString()
+        }
     }
 }
